258066903424da01505619ad311820fa2d7ef495
Naomi <naomistrall@hotmail.com>
2023-08-17 04:45:37 -0300

    Implementação da lógica base de curtidas e teste com back local configurado

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Timeline from "./Pages/Timeline";
 import SignIn from "./Pages/signin";
 import SignUp from "./Pages/signup";
+import { AuthProvider } from "./Contexts/AuthContext";
 
 export default function App() {
   return (
-
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} /> 
-          <Route path="/timeline" element={<Timeline/>}/>
-
-        </Routes>
-      </BrowserRouter> 
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/timeline" element={<Timeline />} />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
     </>
-
   );
 }
